5035e33306a968d26f6876278b9126577652a6b1
ikhumwa edmond mulera <edmondmulera55@gmail.com>
2023-09-22 14:05:00 +0300

    script

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,8 @@
-exec { 'killmenow':
-  command     => 'pkill killmenow',
-  path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+
+# Define an exec resource to kill the process named "killmenow"
+exec { 'killmenow_process':
+  command     => 'pkill -f killmenow',
+  path        => '/usr/bin:/bin', # Modify this path according to your system's environment
   refreshonly => true,
 }
+
