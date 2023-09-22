6da21fb2480127f0c00c0c0a6e5104d90faf5bb6
ikhumwa edmond mulera <edmondmulera55@gmail.com>
2023-09-22 13:55:51 +0300

    script

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,11 @@
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# 1-install_a_package.pp
+
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+exec { 'install-flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  creates => '/usr/local/lib/python3.8/dist-packages/Flask-2.1.0.dist-info',
+  require => Package['python3-pip'],
 }
