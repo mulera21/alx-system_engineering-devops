5324b7a9ebc6410ef8977b8d77eb923678c1b0a9
ikhumwa edmond mulera <edmondmulera55@gmail.com>
2023-09-26 19:56:57 +0300

    update

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,39 +1,38 @@
-class nginx {
+# Define a class to install and configure Nginx
+class webserver {
   package { 'nginx':
-    ensure => installed,
+    ensure => 'installed',
   }
 
   service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => Package['nginx'],
+    ensure    => 'running',
+    enable    => true,
+    require   => Package['nginx'],
   }
 
+  # Configure the Nginx server block for the root path
   file { '/etc/nginx/sites-available/default':
-    ensure  => present,
-    content => '
-      server {
-        listen 80;
-        server_name _;
-        root /var/www/html;
-        
-        location / {
-          proxy_pass http://localhost:8080;
-        }
-        
-        location /redirect_me {
-          return 301 http://www.example.com/;
-        }
-      }
-    ',
+    ensure  => 'file',
+    content => template('my_module/nginx_config.erb'),
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+  
+  # Create a custom Nginx configuration file for redirection
+  file { '/etc/nginx/sites-available/redirect':
+    ensure  => 'file',
+    content => template('my_module/redirect_config.erb'),
+    require => Package['nginx'],
     notify  => Service['nginx'],
   }
 
-  file { '/var/www/html/index.html':
-    ensure  => present,
-    content => 'Hello World!',
-    notify  => Service['nginx'],
+  # Enable the redirect site
+  file { '/etc/nginx/sites-enabled/redirect':
+    ensure  => 'link',
+    target  => '/etc/nginx/sites-available/redirect',
+    require => File['/etc/nginx/sites-available/redirect'],
   }
 }
 
-include nginx
+# Include the class to apply the Nginx configuration
+include webserver
